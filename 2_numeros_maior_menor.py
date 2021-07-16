d058121bcbfde994f48b2637ecd3a84bd7bbfdaa
= <=>
2021-07-16 17:16:34 -0300

    descobrir qual numero é maior e qual é o menor

---
+++ 2_numeros_maior_menor.py
@@ -0,0 +1,8 @@
+numero1 = int(input("escreva o primeiro numero: "))
+numero2 = int(input("escreva o segundo numero: "))
+
+if numero1 > numero2 :
+    print( "o maior numero é:",numero1 )
+
+else :
+    print( "o maior numero é:",numero2 )
